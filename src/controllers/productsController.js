9b0284e5b4e154ecd8fd5ab0e49c4a5b80430542
Dayanna <dayramirezvallejos@gmail.com>
2022-05-09 15:28:26 -0300

    agregar producto

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,24 +1,65 @@
-const productos= require('../data/products.json')
-module.exports= {
-    index: (req,res)=>{
-        res.render('productos',{
+const fs = require('fs'); 
+const path= require('path'); 
+const productos = require('../data/products.json');
+module.exports = {
+    index: (req, res) => {
+        res.render('productos', {
             productos
         })
 
     },
-    detail:(req,res)=>{
-        const {name,id,price, description,category, img1, img2}= (productos.find(producto=>producto.id === +req.params.id)); 
+    detail: (req, res) => {
+        const {
+            name,
+            id,
+            price,
+            description,
+            category,
+            img1,
+            img2
+        } = (productos.find(producto => producto.id === +req.params.id));
 
         res.render('productDetail', {
-            name,id,price, description,category, img1, img2
+            name,
+            id,
+            price,
+            description,
+            category,
+            img1,
+            img2
         })
     },
-    search:(req,res)=>{
-        const {keyword}= req.query; 
-        const resultados= productos.filter(producto=>producto.name.toLowerCase().includes(keyword.toLowerCase())); 
-        res.render('results',{
+    search: (req, res) => {
+        const {
+            keyword
+        } = req.query;
+        const resultados = productos.filter(producto => producto.name.toLowerCase().includes(keyword.toLowerCase()));
+        res.render('results', {
             resultados,
             keyword
         })
+    },
+    add: (req, res) => {
+        res.render('addProduct')
+    },
+    store: (req, res) => {
+        const {name, category, description, price}= req.body;
+        const id= productos.length+1;
+
+        const producto = {
+            id,
+            name, 
+            price, 
+            description, 
+            category, 
+            img1: "noimage.jpg",
+            img2: "noimage.jpg"
+        }
+        
+        productos.push(producto)
+
+        fs.writeFileSync(path.resolve(__dirname,'..','data','products.json'),JSON.stringify(productos,null,3),'utf-8')
+        return res.redirect('/')
     }
+
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,9 +1,11 @@
 const express= require('express'); 
 const router= express.Router(); 
 
-const {index, detail,search}= require('../controllers/productsController'); 
+const {index, detail,search, add, store}= require('../controllers/productsController'); 
 router
 .get('/', index)
 .get('/detail/:id', detail)
 .get('/results', search)
+.get('/add', add)
+.post('/add', store)
 module.exports= router; 
